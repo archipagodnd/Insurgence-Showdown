14daf0027943c2962e9aba24986aef6d1d23c978
Poilerwags <55769238+Poilerwags@users.noreply.github.com>
2021-07-13 01:05:07 +0200

    BIG brains

--- data/moves.ts
+++ data/moves.ts
@@ -12022,11 +12022,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 					return false;
 				}
 			} else {
-				opponent = opponentID.name;
-
-				if (!pokemon.transformInto(opponent)) {
-					return false;
-				}
+				pokemon.formeChange(opponentID);
 			}
 		},
 		secondary: null,
