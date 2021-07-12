478029768c27dc18e55eb44827d6b840946a338d
Poilerwags <55769238+Poilerwags@users.noreply.github.com>
2021-07-12 19:51:37 +0200

    dumb

--- test/sim/abilities/chlorofury.js
+++ test/sim/abilities/chlorofury.js
@@ -12,8 +12,8 @@ describe('Chlorofury', function () {
 
 	it(`should give 1 spatt boost for each faint, should remove after 2 turns`, function () {
 		battle = common.createBattle();
-		battle.setPlayer('p1', {team: [{species: "Cresselia", moves: ['lunardance']}, {species: 'Ludicolo', moves: ['lunardance']}, {species: 'Lotad', ability: ['chlorofury'], moves: ['protect']}]}),
-		battle.setPlayer('p2', {team: [{species: "Mew", moves: ['protect']}]}),
+		battle.setPlayer('p1', {team: [{species: "Cresselia", moves: ['lunardance']}, {species: 'Ludicolo', moves: ['lunardance']}, {species: 'Lotad', ability: ['chlorofury'], moves: ['protect']}]});
+		battle.setPlayer('p2', {team: [{species: "Mew", moves: ['protect']}]});
 
 		battle.makeChoices('move lunardance', 'move protect');
 		battle.makeChoices('move lunardance', 'move protect');
@@ -30,8 +30,8 @@ describe('Chlorofury', function () {
 
 	it(`should not boost if no pokemon have fainted on your side`, function () {
 		battle = common.createBattle();
-		battle.setPlayer('p1', {team: [{species: 'Lotad', ability: ['chlorofury'], moves: ['protect']}, {species: "Cresselia", moves: ['lunardance']}, {species: 'Ludicolo', moves: ['lunardance']}]}),
-		battle.setPlayer('p2', {team: [{species: "Mew", moves: ['protect']}]}),
+		battle.setPlayer('p1', {team: [{species: 'Lotad', ability: ['chlorofury'], moves: ['protect']}, {species: "Cresselia", moves: ['lunardance']}, {species: 'Ludicolo', moves: ['lunardance']}]});
+		battle.setPlayer('p2', {team: [{species: "Mew", moves: ['protect']}]});
 
 		assert.statStage(battle.p1.active[0], 'spe', 0);
 		assert.statStage(battle.p1.active[0], 'spa', 0);
