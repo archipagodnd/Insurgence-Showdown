9ead0c52e0e7e26a897809900392124b03019f24
Poilerwags <55769238+Poilerwags@users.noreply.github.com>
2021-07-13 17:55:51 +0200

    I am quite the genius if I do say so myself

--- config/formats.ts
+++ config/formats.ts
@@ -53,6 +53,7 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 8] Level 120 NatDex OU",
 		mod: 'gen8',
+		maxlevel: 120,
 		defaultlevel: 120,
 		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Moves Clause', 'Species Clause', 'Dynamax Clause', 'Sleep Clause Mod'],
 		banlist: [
@@ -68,12 +69,14 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 8] Level 120 NatDex AG",
 		mod: 'gen8',
+		maxlevel: 120,
 		defaultlevel: 120,
 		ruleset: ['Standard NatDex'],
 	},
 	{
 		name: "[Gen 7] Level 120 OU",
 		mod: 'gen7',
+		maxlevel: 120,
 		defaultlevel: 120,
 		ruleset: ['Standard'],
 		banlist: ['Uber', 'Arena Trap', 'Power Construct', 'Shadow Tag', 'Baton Pass'],
@@ -81,12 +84,14 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 7] Level 120 AG",
 		mod: 'gen7',
+		maxlevel: 120,
 		defaultlevel: 120,
 		ruleset: ['Obtainable', 'Team Preview', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause'],
 	},
 	{
 		name: "[Gen 6] Level 120 OU",
 		mod: 'gen6',
+		maxlevel: 120,
 		defaultlevel: 120,
 		ruleset: ['Standard', 'Swagger Clause'],
 		banlist: ['Uber', 'Arena Trap', 'Shadow Tag', 'Soul Dew', 'Baton Pass'],
@@ -94,6 +99,7 @@ export const Formats: FormatList = [
 	{
 		name: "[Gen 6] Level 120 AG",
 		mod: 'gen6',
+		maxlevel: 120,
 		defaultlevel: 120,
 		ruleset: ['Obtainable', 'Team Preview', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod'],
 	},
