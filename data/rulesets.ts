71405ee607804bc6c42f5ff054685e04f6e55bcd
Poilerwags <55769238+Poilerwags@users.noreply.github.com>
2021-07-17 18:36:52 +0200

    YES

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -110,11 +110,7 @@ export const Rulesets: {[k: string]: FormatData} = {
 			if (!set.item) return;
 			const item = this.dex.items.get(set.item);
 			if (!item.isNonstandard) return;
-			if ([
-				'Past',
-				'PastMove',
-				'Unobtainable',
-			].includes(item.isNonstandard) && !item.zMove && !item.itemUser && !item.forcedForme) {
+			if (['Past', 'Unobtainable'].includes(item.isNonstandard) && !item.zMove && !item.itemUser && !item.forcedForme) {
 				if (this.ruleTable.has(`+item:${item.id}`)) return;
 				return [`${set.name}'s item ${item.name} does not exist in Gen ${this.dex.gen}.`];
 			}
