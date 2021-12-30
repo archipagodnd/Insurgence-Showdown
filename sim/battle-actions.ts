abd17e031610373cff4806401b8312d73aad12bc
Poilerwags <55769238+Poilerwags@users.noreply.github.com>
2021-12-30 10:52:52 +0100

    syntax fixes 3

--- sim/battle-actions.ts
+++ sim/battle-actions.ts
@@ -1771,10 +1771,7 @@ export class BattleActions {
 			let altForme = this.dex.species.get(illusionMon.species.otherFormes);
 			if (altForme[0]) {
 				altForme = this.dex.species.get(illusionMon.species.otherFormes[0]);
-				if (
-					altForme && (altForme.name.slice(altForme.name.length - 5) === "-Mega" ||
-					altForme.name.slice(altForme.name.length - 7) === "-Mega-X")
-				) {
+				if (altForme && (altForme.endsWith("-Mega") || altForme.endsWith("-Mega-X"))) {
 					let gender = pokemon.illusion.details.slice(pokemon.illusion.details.length - 3);
 					if (gender !== ", F" && gender !== ", M") {
 						gender = "";
